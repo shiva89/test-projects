5b9be08579d26e13d6fd6f17ad99556feb074fd6
shiva89 <shiva.pole@talentica.com>
2016-04-21 19:40:24 +0530

    angular2 todo

---
+++ todo/app/TodoList.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Todo App test</h1>
+    <div>
+    <todo-list></todo-list>
+    `,
+    directives: []
+})
+export class TodoList {
+
+
+}
---
+++ todo/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Todo App test</h1>
+    <div>[TodoList]</div>
+    `
+})
+export class AppComponent { 
+    
+    
+}
